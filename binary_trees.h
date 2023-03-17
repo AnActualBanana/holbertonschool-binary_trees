c471fec68036513efd1770c0336d68fcd59a81a1
AnActualBanana <loganmcclain59@gmail.com>
2023-03-17 11:02:11 -0500

    header add

---
+++ binary_trees.h
@@ -0,0 +1,20 @@
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
+
+/* Libraries */
+#include <stdlib.h>
+
+/* Structs */
+typedef struct binary_tree_s binary_tree_t;
+struct binary_tree_s
+{
+    int n;
+    binary_tree_t *parent;
+    binary_tree_t *left;
+    binary_tree_t *right;
+};
+
+/* Prototypes */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+
+#endif /* BINARY_TREES_H */
