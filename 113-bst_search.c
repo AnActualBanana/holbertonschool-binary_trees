7fe46d0151b46a3270a735030980c34685969fed
AnActualBanana <loganmcclain59@gmail.com>
2023-04-04 14:39:18 -0500

    BST - Search function

---
+++ 113-bst_search.c
@@ -0,0 +1,14 @@
+bst_t *bst_search(const bst_t *tree, int value)
+{
+    if (tree == NULL) {
+        return NULL;
+    }
+
+    if (value == tree->n) {
+        return (bst_t *)tree;
+    } else if (value < tree->n) {
+        return bst_search(tree->left, value);
+    } else {
+        return bst_search(tree->right, value);
+    }
+}
