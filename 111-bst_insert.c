eea96d678ca4f6d89e66c1bc1263ee2386f3ee1b
AnActualBanana <loganmcclain59@gmail.com>
2023-04-04 14:35:52 -0500

    typedef in header file

--- 111-bst_insert.c
+++ 111-bst_insert.c
@@ -1,56 +1,57 @@
 #include "binary_trees.h"
 
 /**
- * bst_insertion - function to insert the node
- *
- * @tree: pointer to the root node of the tree to check
- * @new: the value to store
- * Return: 0 or 1
- */
-int bst_insertion(bst_t *tree, bst_t *new)
-{
-	if (tree->n == new->n)
-		return (0);
-	if (tree->n > new->n)
-	{
-		if (tree->left)
-			return (bst_insertion(tree->left, new));
-		new->parent = tree;
-		tree->left = new;
-	}
-	else if (tree->n < new->n)
-	{
-		if (tree->right)
-			return (bst_insertion(tree->right, new));
-		new->parent = tree;
-		tree->right = new;
-	}
-
-	return (1);
-}
-
-/**
- * bst_insert - insert a value in a BST
- *
- * @tree: pointer to the root node of the tree to check
- * @value: the value to store
- * Return: the new tree
+ * bst_insert - inserts a value in a Binary Search Tree
+ * @tree: double pointer to the root node of the BST to insert the value
+ * @value: value to store in the node to be inserted
+ * Return: pointer to the created node, or NULL on failure
  */
 bst_t *bst_insert(bst_t **tree, int value)
 {
-	bst_t *new;
+    bst_t *node, *current;
 
-	new = binary_tree_node(*tree, value);
-	if (new == NULL)
-		return (NULL);
+    if (!tree)
+        return (NULL);
 
-	if (*tree == NULL)
-		*tree = new;
-	else if (bst_insertion(*tree, new) == 0)
-	{
-		free(new);
-		return (NULL);
-	}
+    if (!(*tree))
+    {
+        node = binary_tree_node(NULL, value);
+        if (!node)
+            return (NULL);
+        *tree = node;
+        return (node);
+    }
 
-	return (new);
+    current = *tree;
+    while (current)
+    {
+        if (value == current->n)
+            return (NULL);
+
+        if (value < current->n)
+        {
+            if (!current->left)
+            {
+                node = binary_tree_node(current, value);
+                if (!node)
+                    return (NULL);
+                current->left = node;
+                return (node);
+            }
+            current = current->left;
+        }
+        else
+        {
+            if (!current->right)
+            {
+                node = binary_tree_node(current, value);
+                if (!node)
+                    return (NULL);
+                current->right = node;
+                return (node);
+            }
+            current = current->right;
+        }
+    }
+    return (NULL);
 }
--- binary_trees.h
+++ binary_trees.h
@@ -13,6 +13,8 @@ struct binary_tree_s
     binary_tree_t *left;
     binary_tree_t *right;
 };
+typedef struct binary_tree_s binary_tree_t;
+typedef struct binary_tree_s bst_t;
 
 /* Prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
